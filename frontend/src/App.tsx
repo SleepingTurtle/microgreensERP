0e7e613b5642aaff036f69054f9bf30ed5a5c3f3
Joshua Charles <josh@asleepingturtle.com>
2024-03-29 22:01:00 -0400

    Added nginx conf for deployment

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import { Button } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <h1>HomePage</h1>
+      <Button><Link to="/planting">Planting</Link></Button>
     </>
   );
 }
