529f4375a83352eb595f734e80e2111641770154
Joshua Charles <josh@asleepingturtle.com>
2024-04-05 19:01:29 -0400

    Footer Updated

--- frontend/src/components/ui/Footer.tsx
+++ frontend/src/components/ui/Footer.tsx
@@ -1,5 +1,32 @@
-export default function Footer() {
+import React from 'react'
+import {
+    Typography,
+    Link,
+    Container,
+    Paper,
+} from '@mui/material'
+
+function Footer() {
     return(
-        <p>Footer</p>
+        <Paper sx={{ 
+            position: 'fixed',
+            bottom: 0,
+            width: '100%'
+        }} component='footer'>
+            <Container>
+                <Typography variant='body1'>
+                   Microgreen ERP 
+                </Typography>
+                <Typography variant='body2'>
+                    © {new Date().getFullYear()} MicroGreen ERP
+                </Typography>
+                <Typography variant="body2">
+                    <Link href="#">Privacy Policy</Link> | <Link href="#">Terms of Service</Link>
+                </Typography>
+            </Container>
+
+        </Paper>
     )
 }
+
+export default Footer;
