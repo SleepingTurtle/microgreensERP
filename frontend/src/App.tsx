23806b498feafc8b61b8d6d4bfa837470ad732af
Joshua Charles <josh@asleepingturtle.com>
2024-04-05 19:11:07 -0400

    app updated for planting

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import './App.css';
-import { Button } from '@mui/material';
+import { 
+  Button,
+  Container,
+} from '@mui/material';
 import { Link } from 'react-router-dom';
 
 function App() {
   return (
-    <>
+    <Container>
       <h1>HomePage</h1>
-      <Button><Link to="/planting">Planting</Link></Button>
-    </>
+      <Button><Link to="/growing">Planting</Link></Button>
+    </Container>
   );
 }
 
