3e6f48b52174e84a0f132cb24dcced1a59ae06c7
Joshua Charles <josh@asleepingturtle.com>
2024-06-06 02:17:08 -0400

    Modified Header to to remove nav button
    * Also moved pages into header nav

--- frontend/src/components/ui/Header.tsx
+++ frontend/src/components/ui/Header.tsx
@@ -4,31 +4,18 @@ import {
     Toolbar, 
     Typography,
     Box,
-    IconButton,
     Button,
 } from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu"
 import { Link } from 'react-router-dom';
-
 
 export default function Header() {
     return (
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position='static'>
                 <Toolbar>
-                    <IconButton
-                      size="large"
-                      edge="start"
-                      color="inherit"
-                      aria-label="menu"
-                      sx={{ mr: 2 }} 
-                        >
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" sx={{ flexGrow: 1 }}>
-                        <Link to="/">Microgreens ERP</Link>
-                    </Typography>
-                    <Button color='inherit'>Admin</Button>
+                    <Button color='inherit' href='/'>Microgreens ERP</Button>
+                    <Button color='inherit' href='/growing'>Planting</Button>
+                    <Button color='inherit' href='#'>Inventory</Button>
                 </Toolbar>
             </AppBar>
         </Box>
